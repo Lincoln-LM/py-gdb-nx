91dc5cdd33b485afe3f824d3cfc5270ba1563e48
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2022-06-13 17:36:45 -0600

    Add shield spawn event example

--- examples/README.md
+++ examples/README.md
@@ -1,3 +1,7 @@
 ## pygdbnx examples
 * ``bd_tick_event`` An example of calling a function once a breakpoint is hit. This will print out "Tick event!" each time an in-game tick happens in Pokemon: Brilliant Diamond.
 * ``sh_rng_watch`` An example of calling a function once a memory address is accessed. This will print out the global rng state any time it is accessed by the game in Pokemon: Sword and Shield.
+* ``sh_spawn_event`` An example of reading advanced information when a breakpoint is hit. This will break whenever an overworld pokemon is spawned in Pokemon: Shield, and print out all of its information, which is stored at a register's address.
+
+## helper scripts
+* ``pokemonenums`` This is used to convert the numbers accessed by ``sh_spawn_event`` to their human-readable equivalents.
---
+++ examples/sh_spawn_event.py
@@ -0,0 +1,47 @@
+"""An example of reading advanced information when a breakpoint is hit"""
+# pylint: disable=import-error, wrong-import-position, unused-argument
+import sys
+# exit examples directory
+sys.path.append("../")
+
+from examples.pokemonenums import Shininess, OverworldGender, Nature, Species, Move, Mark, ITEMS
+from pygdbnx.gdbprocess import GdbProcess
+from pygdbnx.breakpoint import Breakpoint
+
+def overworld_spawn_event(gdbprocess: GdbProcess, bkpt: Breakpoint):
+    """Function to be called when an overworld pokemon is generated"""
+    pokemon_addr = gdbprocess.read_register("x20")
+    species = Species(gdbprocess.read_int(pokemon_addr, size = "w"))
+    shininess = Shininess(gdbprocess.read_int(pokemon_addr + 8, size = "w"))
+    nature = Nature(gdbprocess.read_int(pokemon_addr + 12, size = "h"))
+    gender = OverworldGender(gdbprocess.read_int(pokemon_addr + 16, size = "w"))
+    ability = gdbprocess.read_int(pokemon_addr + 20, size = "w")
+    held_item = ITEMS[gdbprocess.read_int(pokemon_addr + 28, size = "h")]
+    egg_move = Move(gdbprocess.read_int(pokemon_addr + 32, size = "w"))
+    guaranteed_ivs = gdbprocess.read_int(pokemon_addr + 53, size = "b")
+    mark = Mark(gdbprocess.read_int(pokemon_addr + 72, size = "h"))
+    brilliant_value = gdbprocess.read_int(pokemon_addr + 74, size = "b")
+    fixed_seed = gdbprocess.read_int(pokemon_addr + 80, size = "g")
+    print(
+        f"{pokemon_addr=:X}",
+        f"{species=} {shininess=}",
+        f"{nature=} {gender=} {ability=} {held_item=}",
+        f"{brilliant_value=} {egg_move=} {guaranteed_ivs=}",
+        f"{mark=} {fixed_seed=:X}",
+        sep="\n",
+        end="\n\n",
+        )
+
+# IP of switch
+gdb_process = GdbProcess("192.168.0.18")
+# create breakpoint at address 7100D317BC
+# (near end of overworld pokemon generation function in Shield)
+gdb_process.add_breakpoint(Breakpoint(
+    0x7100D317BC,
+    "Overworld Pokemon Generated",
+    on_break = overworld_spawn_event
+    ))
+# connecting with gdb automatically pauses execution, resume in order to wait for breakpoints
+gdb_process.resume_execution()
+# start loop of waiting for breaks
+gdb_process.wait_for_break()
